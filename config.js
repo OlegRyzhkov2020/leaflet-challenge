cedf2dc6182a2992e6e99c585cbd714f77cf728e
OlegRyzhkov2020 <61919810+OlegRyzhkov2020@users.noreply.github.com>
2020-10-05 12:21:01 -0500

    mofified earthquakes layers

--- config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "sk.eyJ1Ijoib3J5emhrb3YiLCJhIjoiY2tmb20yM3lkMGJjMDJ5bjV6MHh1azdsYyJ9.DwZfuF0yYCWYrgMrQ-cLMw"
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,8 @@
+// Define a markerSize function that will give each city a different radius based on its population
+function markerSize(depth) {
+  return (depth+1) * 1000;
+}
+
 // Create the tile layer that will be the background of our map
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -7,23 +12,19 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
 });
 // Initialize all of the LayerGroups we'll be using
 var layers = {
-  COMING_SOON: new L.LayerGroup(),
-  EMPTY: new L.LayerGroup(),
-  LOW: new L.LayerGroup(),
-  NORMAL: new L.LayerGroup(),
-  OUT_OF_ORDER: new L.LayerGroup()
+  EARTHQUAKES: new L.LayerGroup(),
+  EARTHQUAKES_CIRCLE: new L.LayerGroup(),
+  TECTONIC_PLATES: new L.LayerGroup()
 };
 
 // Create the map with our layers
-var map = L.map("mapid", {
-  center: [40.73, -74.0059],
-  zoom: 12,
+var map = L.map("map-id", {
+  center: [39.8097343, -98.5556199],
+  zoom: 3,
   layers: [
-    layers.COMING_SOON,
-    layers.EMPTY,
-    layers.LOW,
-    layers.NORMAL,
-    layers.OUT_OF_ORDER
+    layers.EARTHQUAKES,
+    layers.EARTHQUAKES_CIRCLE,
+    layers.TECTONIC_PLATES
   ]
 });
 
@@ -32,11 +33,9 @@ lightmap.addTo(map);
 
 // Create an overlays object to add to the layer control
 var overlays = {
-  "Coming Soon": layers.COMING_SOON,
-  "Empty Stations": layers.EMPTY,
-  "Low Stations": layers.LOW,
-  "Healthy Stations": layers.NORMAL,
-  "Out of Order": layers.OUT_OF_ORDER
+  "EARTHQUAKES": layers.EARTHQUAKES,
+  "EARTHQUAKES CIRCLES": layers.EARTHQUAKES_CIRCLE,
+  "TECTONIC PLATES": layers.TECTONIC_PLATES
 };
 
 // Create a control for our layers, add our overlay layers to it
@@ -55,13 +54,82 @@ info.onAdd = function() {
 // Add the info legend to the map
 info.addTo(map);
 
+// Initialize an object containing icons for each layer group
+var icons = {
+  EARTHQUAKES: L.ExtraMarkers.icon({
+    icon: "ion-settings",
+    iconColor: "white",
+    markerColor: "darkred",
+    shape: "circle"
+  }),
+  TECTONIC_PLATES: L.ExtraMarkers.icon({
+    icon: "ion-android-bicycle",
+    iconColor: "white",
+    markerColor: "red",
+    shape: "star"
+  })
+};
+
 // Perform an API call to the Citi Bike Station Information endpoint
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(infoRes) {
 
   // When the first API call is complete, perform another call to the Citi Bike Station Status endpoint
   //d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_status.json", function(statusRes) {
     var data_features = infoRes.features;
-    console.log(data_features)
+    var lat, lon, location, depth, updatedAt;
+    console.log(data_features);
+    console.log(data_features[0].properties);
+    // Create an object to keep of the number of markers in each layer
+    var stationCount = {
+      EARTHQUAKES: 0,
+      EARTHQUAKES_CIRCLE: 0,
+      TECTONIC_PLATES: 0
+    };
+    var stationStatusCode = "EARTHQUAKES";
 
+    for (var i=0; i< data_features.length; i++) {
+        // Update the station count
+        stationCount[stationStatusCode]++;
+
+        updatedAt = data_features[i].properties.updated;
+        lon = data_features[i].geometry.coordinates[0];
+        lat = data_features[i].geometry.coordinates[1];
+        depth = data_features[i].geometry.coordinates[2];
+        location = [lat,lon];
+
+        var newCircle = L.circle(location, {
+          fillOpacity: 0.75,
+          color: "black",
+          fillColor: "darkgreen",
+          // Setting our circle's radius equal to the output of our markerSize function
+          // This will make our marker's size proportionate to its population
+          radius: markerSize(depth)
+        });
+
+        // Create a new marker with the appropriate icon and coordinates
+        var newMarker = L.marker([lat, lon], {
+          icon: icons[stationStatusCode]
+        });
+        // Add the new marker to the appropriate layer
+        newCircle.addTo(layers["EARTHQUAKES_CIRCLE"]);
+        newMarker.addTo(layers[stationStatusCode]);
+
+        // Bind a popup to the marker that will  display on click. This will be rendered as HTML
+        newMarker.bindPopup(data_features[i].id + "<br> Magnitude: " + data_features[i].properties.mag + "<br>" + "Place:" +data_features[i].properties.place);
+        newCircle.bindPopup(data_features[i].id + "<br> Magnitude: " + data_features[i].properties.mag + "<br>" + "Place:" +data_features[i].properties.place);
+
+        // Call the updateLegend function, which will... update the legend!
+        updateLegend(updatedAt, stationCount);
+
+      }
 
 });
+
+// Update the legend's innerHTML with the last updated time and station count
+function updateLegend(time, stationCount) {
+  document.querySelector(".legend").innerHTML = [
+    "<p>Last Update: " + moment.unix(time).format("h:mm:ss A") + "</p>",
+    "<p class='out-of-order'>Last Week Number of Earthquakes: " + stationCount.EARTHQUAKES + "</p>",
+    "<p class='coming-soon'>Number of Tectonic Plates: " + stationCount.TECTONIC_PLATES + "</p>",
+  ].join("");
+}
